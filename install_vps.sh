380b6b61767152061c04cc9a1066f606dba720f1
Mateusz Kiliński <mateusz.kilinski@gmail.com>
2020-11-20 19:05:33 +0100

    install vps script optimalized

--- install_vps.sh
+++ install_vps.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-apt -y update
+# upgrade apt and basic packages
+apt update
 apt -y upgrade
-apt -y install docker.io python3-pip
+
+# install basic components
+apt -y install zsh docker.io python3-pip
 pip3 install docker-compose
+
+# install ohmyzsh shell 
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 mkdir -p /srv/proxy/ssl
