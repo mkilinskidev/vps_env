e89b94e7bd8e874382384e39d1a9249a94252fd1
Mateusz Kiliński <mateusz.kilinski@gmail.com>
2020-12-04 13:50:34 +0100

    fix oh-my-zsh

--- install_vps.sh
+++ install_vps.sh
@@ -8,8 +8,6 @@ apt -y upgrade
 apt -y install zsh docker.io python3-pip fail2ban
 pip3 install docker-compose
 
-# install ohmyzsh shell 
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 mkdir -p /srv/proxy/ssl
 
@@ -18,3 +16,6 @@ cp /srv/backend/fail2ban/jail.local /etc/fail2ban/
 
 systemctl start fail2ban
 systemctl enable fail2ban
+
+# install ohmyzsh shell [thos should be always at the end]
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
